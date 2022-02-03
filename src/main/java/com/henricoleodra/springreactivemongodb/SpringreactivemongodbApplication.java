0fd88d04e6626749464d8d053f562a3a034c6684
Henrico Leodra <leodra28@gmail.com>
2022-02-03 11:50:45 +0700

    [INIT] Spring Boot Reactive

---
+++ src/main/java/com/henricoleodra/springreactivemongodb/SpringreactivemongodbApplication.java
@@ -0,0 +1,13 @@
+package com.henricoleodra.springreactivemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringreactivemongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringreactivemongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henricoleodra/springreactivemongodb/SpringreactivemongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henricoleodra.springreactivemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringreactivemongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
